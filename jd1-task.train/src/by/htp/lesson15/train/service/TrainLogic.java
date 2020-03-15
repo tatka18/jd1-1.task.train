e1067fbb280a2485dccf8a2f8382b61cd4e44aff
tatka18 <60408012+tatka18@users.noreply.github.com>
2020-03-15 20:11:17 +0300

    Update TrainLogic.java

--- jd1-task.train/src/by/htp/lesson15/train/service/TrainLogic.java
+++ jd1-task.train/src/by/htp/lesson15/train/service/TrainLogic.java
@@ -1,5 +1,6 @@
 package by.htp.lesson15.train.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import by.htp.lesson15.train.bean.Train;
@@ -33,18 +34,19 @@ public class TrainLogic {
 				}
 
 			}
-			
+
 			// запоминаем значение, адрес объекта Train
-			Train temp1 = trains.get(i);	//поезд сейчас
-			Train temp2 = trains.get(minTrainNumberIndex);	//Поезд с миниальным индексом
-			
+			Train temp1 = trains.get(i); // поезд сейчас
+			Train temp2 = trains.get(minTrainNumberIndex); // Поезд с миниальным индексом
+
 			// меняем поезда местами//меняем местами коллекции
 			trains.set(i, temp2);
 			trains.set(minTrainNumberIndex, temp1);
-			}
-		
+
+		}
 
 	}
+
 	public void sortByStation(List<Train> trains) {
 
 		for (int i = 0; i < trains.size(); i++) {
@@ -68,6 +70,87 @@ public class TrainLogic {
 
 		}
 
+	}
+
+	public void starting(List<Train> trains) {
+		int from = 0;
+
+		while (from < trains.size()) {
+			List<Integer> dublicate = findDublicateStations(trains, from);
+
+			if (dublicate.size() == 1) {
+				from++;
+
+			} else {
+				from = dublicate.get(1);
+
+				sortByDate(trains, dublicate);
+			}
+
+		}
+
+	}
+
+	public List<Integer> findDublicateStations(List<Train> trains, int from) {
+
+		int end;
+
+		String firstTrain;
+		String secondTrain;
+
+		List<Integer> list = new ArrayList<>();
+		list.add(0, from);
+
+		for (int i = from; i < trains.size() - 1; i++) {
+			firstTrain = trains.get(i).getNameOfTown();
+			secondTrain = trains.get(i + 1).getNameOfTown();
+
+			if (firstTrain.compareTo(secondTrain) == 0) {
+				end = i + 1;
+				list.add(1, end);
+			} else {
+				return list;
+			}
+
+		}
+
+		return list;
+	}
+
+	public void sortByDate(List<Train> trains, List<Integer> list) {
+
+		for (int i = list.get(0); i <= list.get(1); i++) {
+			int trainMinimIndex = i;
+
+			for (int j = i + 1; j < list.get(1); j++) {
+				int trainHour = trains.get(i).getDate().getHour();
+				int trainHourNext = trains.get(j).getDate().getHour();
+				trainMinimIndex = j;
+
+				if (trainHour > trainHourNext) {
+					trainHourNext = trainHour;
+
+				}
+
+				if (trainHour == trainHourNext) {
+					int trainMinute = trains.get(i).getDate().getMinute();
+					int trainMinuteNext = trains.get(j).getDate().getMinute();
+
+					if (trainMinute > trainMinuteNext) {
+						trainHourNext = trainHour;
+
+					}
+
+				}
+				Train tr1 = trains.get(i);
+				Train tr2 = trains.get(trainMinimIndex);
+
+				trains.set(trainMinimIndex, tr1);
+				trains.set(i, tr2);
+
+			}
+
+		}
 	}
 
 }
